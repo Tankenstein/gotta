1cb48d8b2738cd3167c3b36d71b242eac20bc951
Uku Tammet <ukutammet@gmail.com>
2015-03-22 03:24:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+gotta - A feisty, minimal command line todos manager.
+-----------------------------------------------------
+
+I sometimes feel like super quickly jutting down something i gotta do.
+I'm in the terminal a lot, so i felt like i should be able to do that
+in the terminal.
+
+Installing
+----------
+```
+npm install gotta -g
+```
+Maybe you gotta add sudo, maybe you don't.
+Run
+```
+gotta --version
+```
+to see if it's installed.
+
+Usage
+-----
+```
+gotta [options] [command]
+```
+Commands:
+```
+do <task>      Add a task you gotta do.
+did <task>  Remove a task you did.
+what        Show tasks you gotta do.
+```
+Options:
+```
+-h, --help     output usage information
+-V, --version  output the version number
+```
+So for instance, if i gotta write some tests for gotta, i would go:
+```
+gotta do "write some tests for gotta"
+```
+Or do the laundry:
+```
+gotta do "the laundry"
+```
+Whatever you gotta do man.
+Later, when i want to see what i gotta do, i would go:
+```
+gotta what
+```
+And if i did the laundry, i would go:
+```
+gotta did "laundry"
+```
+Removing a task doesn't need the full task, just a part of it.
+
+Side note: gotta is awesome when you call it from .bashrc, to show what
+you gotta do today.
+
+Things i gotta add
+------------------
+A fancy user configuration utility, where you can specify what the commands
+are and what sort of language to use.
+
+Gotta should whine when you leave something not done for a while.
+
+A special version of `gotta need` for when you're running it from .bashrc, 
+aka when you want it to show anytime you log onto terminal.
+
+Screenshots to readme.
+
+If you have any more fun ideas, send them my way or make a pull request.
+
+License
+-------
+MIT
+
+Contributors
+------------
+Seriously? i'm adding a contributors on this?
+
+Uku Tammet
+...
---
+++ gotta.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+/* jslint node:true */
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+
+var commander = require('commander');
+var colors = require('colors');
+
+var pkg = require( path.join(__dirname, 'package.json') );
+
+var tasks;
+
+/**
+ * Try to parse gotta tasks file, if fails, create new one.
+ */
+try {
+	tasks = require( path.join(__dirname, 'tasks.json') );
+} catch (error) {
+	fs.writeFileSync( 'tasks.json' , '[]');
+	tasks = require( path.join(__dirname, 'tasks.json') );
+}
+
+/**
+ * Task object.
+ * @param {string} taskName Name of the task.
+ */
+var Task = function(taskName) {
+	this.name = taskName;
+	this.createdAt = Date.now();
+};
+
+/**
+ * Add a task to tasks.
+ * @param {string} taskName The name of the task to add.
+ */
+var addTask = function(taskName) {
+	var task = new Task(taskName);
+	tasks.push(task);
+	console.log('You gotta ' + taskName.green + '!');
+};
+
+/**
+ * Remove a task from tasks.
+ * @param {string} taskNamePart A part of the name of the todo to remove. 
+ */
+var removeTask = function(taskNamePart) {
+
+	var found = false;
+	var removeTask = taskNamePart.toLowerCase();
+	tasks.forEach(function(task, index) {
+		var taskName = task.name.toLowerCase();
+		if (taskName.indexOf(removeTask) != -1) {
+			console.log('Nice! you did: ' + tasks[index].name.green);
+			tasks.splice(index, 1);
+			found = true;
+		}
+	});
+	if (!found) {
+		console.log("Didn't find anything like " + taskNamePart.red + ' that you gotta do.');
+	}
+};
+
+/**
+ * Show tasks needed to do.
+ */
+var show = function() {
+	if (tasks.length) {
+		console.log('You gotta');
+		tasks.forEach(function(task) {
+			console.log('    ' + task.name.green);
+		});
+		console.log('\nGet to work!');
+	} else {
+		console.log("You ain't gotta do " + "nothin".green + '.');
+		console.log('Go on, get!');
+	}
+};
+
+/**
+ * Set up commander.
+ */
+commander
+	.version(pkg.version);
+
+commander
+	.command('do <task>')
+	.description('Add a task you gotta do.')
+	.action(function(task) {
+		console.log(task);
+		addTask(task);
+	});
+
+commander
+	.command('did <task>')
+	.description('Remove a task you did.')
+	.action(function(task) {
+		removeTask(task);
+	});
+
+commander
+	.command('what')
+	.description('Show tasks you gotta do.')
+	.action(function() {
+		show();
+	});
+
+commander.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+	commander.help();
+}
+
+/**
+ * Write tasks to file.
+ */
+fs.writeFileSync(path.join(__dirname, 'tasks.json'), JSON.stringify(tasks, null, 2));
